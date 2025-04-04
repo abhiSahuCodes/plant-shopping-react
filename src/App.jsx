0fd41899da373236798357c966e88ce46f5dae18
abhiSahuCodes <abhishek963sahu@gmail.com>
2025-04-04 17:01:28 +0530

    Add update to react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { store } from './store/store';
-import Header from './components/Header/Header';
-import LandingPage from './components/LandingPage/LandingPage';
-import ProductList from './components/ProductList/ProductList';
-import ShoppingCart from './components/ShoppingCart/ShoppingCart';
-import './App.css';
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
+import Header from "./components/Header/Header";
+import LandingPage from "./components/LandingPage/LandingPage";
+import ProductList from "./components/ProductList/ProductList";
+import ShoppingCart from "./components/ShoppingCart/ShoppingCart";
+import "./App.css";
 
 function App() {
   return (
@@ -18,6 +18,7 @@ function App() {
               <Route path="/" element={<LandingPage />} />
               <Route path="/products" element={<ProductList />} />
               <Route path="/cart" element={<ShoppingCart />} />
+              <Route index element={<LandingPage />} />
             </Routes>
           </main>
         </div>
@@ -26,4 +27,4 @@ function App() {
   );
 }
 
-export default App
+export default App;
